c37cad01f0bbf6d7fcfae0372f023425890d7f50
Jithin K Satheesh <37372507+JithinKSatheesh@users.noreply.github.com>
2019-06-09 13:59:28 +0530

    Add files via upload

---
+++ js/move.js
@@ -0,0 +1,20 @@
+ (function () {
+  var elem = document.getElementById("counter"); 
+  var overlay = document.getElementById("overlay"); 
+  var num = 1;
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (num >= 100) 
+    { 
+      overlay.style.transform = "translate(0px,-100vh)";
+      clearInterval(id);
+
+    }
+    else
+    {
+      num++;
+      elem.innerHTML = num + ' % ';
+
+    }
+  }
+})();
